9843a518471263ad258d44e2201e8e6ca7a9295a
jspencerhopkins <jspencerhopkins@gmail.com>
2015-02-19 15:01:29 -0500

    Add num_to_span & test files

---
+++ num_to_span.rb
@@ -0,0 +1,15 @@
+# Translations on Rails
+
+# 1)  Use TDD to write a method that can translate a number (1,2,3,...1000000) into Spanish.
+
+# 2)  Write a Rails app that can display the numbers and their translations in a list, eg "1 in Spanish is 'uno'". You won't need any model files, just a view and a controller. Put the file you wrote in step 1 into the lib directory in your app so the view template can use the method.
+
+# 3)  Use TDD to write a method that can translate a number into Roman numerals. Put that in the lib directory of your app, and update the view template so it has a two column table, with column headers "Roman Numeral" and "Spanish", and data like "II", "dos".
+
+class Fixnum
+
+	def to_spanish
+
+	end
+
+end
---
+++ num_to_span_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'num_to_span'
+
+class TestNumToSpan < MiniTest::Unit::TestCase
+
+
+
+
+
+
+end
